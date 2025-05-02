22714d55dbf13053c9cee0127c0e0e2778ae03a4
Laszlo Kulcsar <laszlokulcsar86@gmail.com>
2025-05-02 12:55:10 +0200

    modified:   ../../kubepanel-install.yaml 	modified:   watchdog_entrypoint.sh

--- modules/watchdog/watchdog_entrypoint.sh
+++ modules/watchdog/watchdog_entrypoint.sh
@@ -3,18 +3,22 @@
 # Infinite loop
 while true
 do
-    echo "Iterating over directories in /kubepanel/yaml_templates/..."
+  for dir in /kubepanel/yaml_templates/*/; do
+    [ -d "$dir" ] || continue
+    echo "$(date): Iterating over directories in $dir"
+    sleep 5
 
-    # Iterate over directories under /kubepanel/yaml_templates/
-    for dir in /kubepanel/yaml_templates/*/ ; do
-        if [ -d "$dir" ]; then
-            echo "Applying YAMLs in $dir..."
-            sleep 5
-            # Apply YAML files in the current directory
-            kubectl apply -f "$dir"
-            rm -rf "$dir"
-        fi
-    done
+    if [ "$dir" = "/kubepanel/yaml_templates/fwrules/" ]; then
+      echo "→ Patching modsecurity-snippet from all YAMLs in fwrules/"
+      kubectl patch configmap nginx-load-balancer-microk8s-conf -n ingress --type merge --patch "$(cat "${dir}"*.yaml)"
+    else
+      echo "→ Applying all manifests in $dir"
+      kubectl apply -f "$dir"
+    fi
+
+    rm -rf "$dir"
+  done
+
     python3 /kubepanel/manage.py check_status
     # Sleep for 5 seconds between cycles
     sleep 5
