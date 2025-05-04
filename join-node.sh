1ca3f55869bb236803c7f80c275b0d35106eabbc
Laszlo Kulcsar <laszlokulcsar86@gmail.com>
2025-05-04 18:30:36 +0200

    modified:   join-node.sh

--- join-node.sh
+++ join-node.sh
@@ -3,7 +3,7 @@
 sudo systemctl stop multipathd && sudo systemctl disable multipathd
 vgcreate linstorvg /dev/sdb
 lvcreate -l100%FREE -T linstorvg/linstorlv
-sudo apt update && sudo snap install microk8s --classic --channel=1.31
+sudo apt update && apt install lvm2 -y && sudo snap install microk8s --classic --channel=1.31
 echo "MicroK8S has been installed, waiting to be ready..."
 sudo microk8s status --wait-ready
 echo "Please run the cluster join command:"
