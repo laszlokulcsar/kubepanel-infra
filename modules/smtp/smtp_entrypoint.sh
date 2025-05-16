31eccb0432bdcef6e1bd5c826e9422ae8a2f42fc
Laszlo Kulcsar <laszlokulcsar86@gmail.com>
2025-05-16 16:17:27 +0200

    modified:   kubepanel-install.yaml 	modified:   modules/smtp/Dockerfile.smtp 	modified:   modules/smtp/master.cf 	modified:   modules/smtp/smtp_entrypoint.sh

--- modules/smtp/smtp_entrypoint.sh
+++ modules/smtp/smtp_entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 chown vmail:vmail /var/mail/vmail
 rm -rf /var/spool/postfix/pid/*
+rm -f /var/spool/postfix/private/dovecot-lmtp
+rm -f /var/spool/postfix/private/auth
 /etc/init.d/postfix start
 /etc/init.d/dovecot start
 while true; do sleep 36000; done;
